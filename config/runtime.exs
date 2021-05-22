16cc48d3e8fd0a910aa868d1ce151985369c2ced
Patrick Muehlbauer <patrick@opentrash.org>
2021-05-22 19:46:42 +0200

    fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -39,5 +39,5 @@ if config_env() == :prod do
 
   config :supabase,
     base_url: System.get_env("SUPABASE_URL"),
-    api_key: System.get_env("SUPABASE_KEY"
+    api_key: System.get_env("SUPABASE_KEY")
 end
