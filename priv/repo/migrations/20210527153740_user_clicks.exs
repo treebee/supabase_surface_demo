e5ed6c84e58ca948e1dd2d81c9283616cd6c4903
Patrick Muehlbauer <patrick@opentrash.org>
2021-05-28 10:04:17 +0200

    For some reason this doesn't work with the priviliges of the 'postgres' user anymore

--- priv/repo/migrations/20210527153740_user_clicks.exs
+++ priv/repo/migrations/20210527153740_user_clicks.exs
@@ -6,6 +6,10 @@ defmodule SupabaseSurfaceDemo.Repo.Migrations.UserClicks do
       add :user_id, :uuid, null: false
     end
 
-    execute("alter publication supabase_realtime add table user_clicks;")
+
+    # XXX for some reason this doesn't work anymore, fails with
+    # ERROR 42501 (insufficient_privilege) must be owner of publication supabase_realtime
+    # But it worked in the 'init' migration ?!
+    # execute("alter publication supabase_realtime add table user_clicks;")
   end
 end
